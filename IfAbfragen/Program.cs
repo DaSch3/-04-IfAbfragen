96e41fc303752a8250da9d593b13420ecad4844c
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:22:38 +0200

    Projektdateien hinzufügen.

---
+++ IfAbfragen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfAbfragen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 10;
+
+            if (alter >= 18)
+            {
+                Console.WriteLine("Du bist alt genug!");
+            }
+            else
+            {
+                Console.WriteLine("Du bist nicht alt genug!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
